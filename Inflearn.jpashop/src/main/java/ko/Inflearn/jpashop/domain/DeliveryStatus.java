c50e50a586d59973d0b590fcde208ffaf3722cc7
user-PC\user <ko>
2020-12-01 22:13:35 +0900

    프로젝트 리팩토링

--- Inflearn.jpashop/src/main/java/ko/Inflearn/jpashop/domain/DeliveryStatus.java
+++
@@ -1,5 +0,0 @@
-package ko.Inflearn.jpashop.domain;
-
-public enum DeliveryStatus {
-    READY, COMP
-}
--- Inflearn.jpashop/src/main/java/ko/Inflearn/jpashop/domain/OrderStatus.java
+++
@@ -1,5 +0,0 @@
-package ko.Inflearn.jpashop.domain;
-
-public enum OrderStatus {
-    ORDER, CANCEL
-}
